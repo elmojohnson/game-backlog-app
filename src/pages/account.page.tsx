57bef7d0f42d1d6053aa74fdf6ae196ac9e83973
Elmo Johnson <elmoj6@gmail.com>
2025-12-24 03:22:35 +1100

    Simple user info

--- src/pages/account.page.tsx
+++ src/pages/account.page.tsx
@@ -1,13 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { Spinner } from "@/components/ui/spinner";
+import { FullScreenSpinner, Spinner } from "@/components/ui/spinner";
 import { useSignOutMutation } from "@/queries/mutations/auth.mutation";
+import { useCurrentUserQuery } from "@/queries/queries/auth.query";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import { toast } from "sonner";
 
 const Account = () => {
+  const { data: user, isPending, isError, error } = useCurrentUserQuery(); // TODO: Avoid user query duplication from protected route check
   const mutation = useSignOutMutation();
   const navigate = useNavigate();
+
+  if (isPending) {
+    return <FullScreenSpinner />;
+  }
+
+  if (isError) {
+    return <p>{error.message}</p>;
+  }
 
   useEffect(() => {
     if (mutation.isError) {
@@ -20,7 +30,11 @@ const Account = () => {
   }, [mutation.status]);
 
   return (
-    <div>
+    <div className="wrapper py-4">
+      <div className="mb-4">
+        <h5 className="font-bold text-lg">{user?.user_metadata.name}</h5>
+        <span>Email: {user?.email}</span>
+      </div>
       <Button onClick={() => mutation.mutate()} disabled={mutation.isPending}>
         {mutation.isPending && <Spinner />}Sign out
       </Button>
