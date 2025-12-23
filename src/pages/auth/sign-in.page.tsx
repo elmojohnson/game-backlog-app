de75ad90e8400244fe5e91c499eb793e739818cb
Elmo Johnson <elmoj6@gmail.com>
2025-12-24 01:17:16 +1100

    Setup Tailwind and Shadcn

--- src/pages/auth/sign-in.page.tsx
+++ src/pages/auth/sign-in.page.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div>
+      <Button>Sign in</Button>
+    </div>
   )
 }
 
