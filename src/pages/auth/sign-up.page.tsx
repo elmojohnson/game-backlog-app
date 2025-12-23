63d8d02ed1edb6c7c2488f40e077969d16053b55
Elmo Johnson <elmoj6@gmail.com>
2025-12-24 02:15:18 +1100

    Sign up page UI

--- src/pages/auth/sign-up.page.tsx
+++ src/pages/auth/sign-up.page.tsx
@@ -1,9 +1,143 @@
-import React from 'react'
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignUpSchema, type SignUpDto } from "@/schemas/auth.schema";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircleIcon } from "lucide-react";
+import { Spinner } from "@/components/ui/spinner";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  type FormInput = {
+    name: keyof SignUpDto;
+    label: string;
+    placeholder: string;
+    type: string;
+  };
 
-export default SignUp
+  const formInputs: FormInput[] = [
+    {
+      name: "name",
+      label: "Name",
+      placeholder: "Name",
+      type: "text",
+    },
+    {
+      name: "email",
+      label: "Email",
+      placeholder: "Email",
+      type: "email",
+    },
+    {
+      name: "password",
+      label: "Password",
+      placeholder: "Password",
+      type: "password",
+    },
+  ];
+
+  const form = useForm<SignUpDto>({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: SignUpDto) => {
+    if (data.email === "error@test.com") {
+      form.setError("root", { type: "custom", message: "User already exists" });
+    }
+
+    console.log(data);
+  };
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Card className="w-full md:w-75 mx-4 md:mx-0">
+        <CardHeader>
+          <CardTitle>Sign up</CardTitle>
+          <CardDescription>Create an account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            id="sign-up-form"
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-4"
+          >
+            {formInputs.map((input) => {
+              return (
+                <Controller
+                  key={input.label}
+                  name={input.name}
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldLabel htmlFor={input.label + "-input"}>
+                        {input.label}
+                      </FieldLabel>
+                      <Input
+                        {...field}
+                        id={input.label + "-input"}
+                        aria-invalid={fieldState.invalid}
+                        placeholder={input.label}
+                        autoComplete="off"
+                        type={input.type}
+                        disabled={form.formState.isSubmitting}
+                      />
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
+                />
+              );
+            })}
+          </form>
+        </CardContent>
+        <CardFooter className="flex flex-col gap-2">
+          {form.formState.errors.root?.type == "custom" && (
+            <Alert variant="destructive">
+              <AlertCircleIcon />
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>
+                <p>{form.formState.errors.root.message}</p>
+              </AlertDescription>
+            </Alert>
+          )}
+          <Button
+            className="w-full"
+            type="submit"
+            form="sign-up-form"
+            id="sign-up-button"
+            disabled={form.formState.isSubmitting}
+          >
+            {form.formState.isSubmitting && <Spinner />}
+            Sign up
+          </Button>
+          <Button
+            className="w-full"
+            variant="outline"
+            asChild
+            disabled={form.formState.isSubmitting}
+          >
+            <Link to="/auth/sign-in">I already have an account</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default SignUp;
--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -6,3 +6,14 @@ export const SignInSchema = z.object({
 });
 
 export type SignInDto = z.infer<typeof SignInSchema>;
+
+export const SignUpSchema = z.object({
+  name: z.string().nonempty("Name is required"),
+  email: z.email().nonempty("Email is required"),
+  password: z
+    .string()
+    .min(6, "Password is too short")
+    .nonempty("Password is required"),
+});
+
+export type SignUpDto = z.infer<typeof SignUpSchema>;
