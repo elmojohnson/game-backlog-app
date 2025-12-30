31e70876ebac23ca342cec55a328e598a94e6161
Elmo Johnson <elmoj6@gmail.com>
2025-12-30 11:53:49 +1100

    Fix run test and attach envs

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,4 +21,11 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
   ],
+
+  webServer: {
+    command: "npm run preview",
+    port: 5173,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
